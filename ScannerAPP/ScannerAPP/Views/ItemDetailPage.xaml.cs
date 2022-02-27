91ce990e5b927eab204e3e184d8f020661745cca
E.M.A <elennmayadeliine@gmail.com>
2022-02-27 19:45:54 +0200

    Initial commit

---
+++ ScannerAPP/ScannerAPP/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using ScannerAPP.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace ScannerAPP.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
